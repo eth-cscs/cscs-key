3416dedb63c293180dc597ef5bfb7172e6e8d522
Schups <schuppli@cscs.ch>
2026-05-09 14:59:49 +0200

    Updated completion to support --browser

--- src/completion.rs
+++ src/completion.rs
@@ -21,3 +21,21 @@ pub fn generate_completion(args: &CompletionArgs) -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bash_completion_includes_browser_option_and_values() {
+        let mut cmd = crate::Cli::command();
+        let bin_name = cmd.get_name().to_string();
+        let mut output = Vec::new();
+
+        generate(Shell::Bash, &mut cmd, bin_name, &mut output);
+
+        let completion = String::from_utf8(output).expect("completion output should be UTF-8");
+        assert!(completion.contains("--browser"));
+        assert!(completion.contains("firefox chrome safari edge opera"));
+    }
+}
